dfbf54971cf188a5234d4357e5595b928e3b948c
Rhec 1337 <12795928-MisaelChristoph@users.noreply.replit.com>
2024-05-14 08:33:38 +0000

    Pushingg

--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>APP TRAVELS</title>
+    <title>APP Travello</title>
 </head>
 <body>
       <header>
@@ -29,7 +29,7 @@
 
 
       <footer>
-          App by Alessandro Vanoli
+          App by SaelSumenep @ 2024
       </footer>
 </body>
 </html>
